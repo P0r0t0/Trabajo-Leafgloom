45351ca3f55d311daa3027da2efe9da9c3987fb9
P0r0t0 <kz17adv@gmail.com>
2023-11-16 11:08:23 -0600

    se puede genteeee

--- leafgloom/vistas.py
+++ leafgloom/vistas.py
@@ -6,41 +6,41 @@ import requests
 
 # import requests
 
+##
+# se movio la extraccion de datos a la parte logica del codigo
 url = "https://perenual.com/api/species-list?key=sk-24d46532eef6848082612"
 response = requests.get(url)
 if response.status_code == 200:
     datos = response.json()
 else:
     datos = {"error":[]}
-
+##
 
 def Pagina_inicial(request):
 
-   ##doc_externo = open("C:/Users/kz17a/OneDrive/Escritorio/Proyectos de intro/Leafgloom/leafgloom/plantillas/Logo.html")
-   ##plt = Template(doc_externo.read())
-   ##doc_externo.close()
-
-   #doc_externo = loader.get_template("Logo.html") 
-   ##ctx = Context()
-   #documento = doc_externo.render()
-   
    return render(request, "Logo.html") #se puede poner un tercer parametro que seria el contexto, este caso no trae
 
-
-# Se esta intentando que aparezca informacion de la planata selecionadad en listado por cada catalogo
+##
+# se esta intentando que el display de información sea lindo :P
+# la info de la planta se saca llamando entre {{ }} la llave del dic de cada planta del json()
+# posiblemente se cambie el for por un while
 def catalogo(request,nombre_planta):
 
-    doc_externo = loader.get_template("Catalogo.html")
+    dic = {"n_p" : nombre_planta} 
+    for cosa in datos["data"]:
+        if dic["n_p"] == cosa["common_name"]:
+            ctx = cosa
+        
 
-    documento = doc_externo.render({"other_name": nombre_planta })
+    return render(request , "Catalogo.html" , ctx)
+##
 
-    return HttpResponse(documento)
-
-
-
+#
 def listado(request):
 
     return render(request, "listado.html",datos)
+#
+
 
 def plantas(request):
     return render(request, "plantas.html")
